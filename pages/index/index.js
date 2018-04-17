c3eecc09c44ab8fba30830c86f8ce4730a5b885d
Dan Li <dl.phpf@gmail.com>
2018-04-17 17:23:44 +0800

    added top scroll bar on index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    navbar: ['New', 'Trending', 'Search'],
+    currentTab: 0 
   },
+
+  navbarTap: function (e) {
+    this.setData({
+      currentTab: e.currentTarget.dataset.idx
+    })
+  }, 
 
   showGif: function (e) {
     console.log(e);
