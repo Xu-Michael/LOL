f678f35b028ac46d742680af1611d2ff8695ca0b
Leif Copping <seven.minty@yandex.com>
2018-04-22 22:31:25 -0700

    added heart color upon bindtap in showpage

--- pages/show/show.js
+++ pages/show/show.js
@@ -35,65 +35,10 @@ Page({
   },
 
 
-  // collect: function (e) {
-  //   let data = e.currentTarget.dataset;
-  //   let page = this;
-  //   try {
-  //     var user = wx.getStorageSync('user')
-  //     if (user) {
-  //       console.log(user)
-  //       const current_user_id = user.id
-  //       wx.request({
-  //         url: `https://gifme-api.wogengapp.cn/api/v1/users/${user.id}`,
-  //         // url: `http://localhost:3000/api/v1/users/${current_user_id}`,
-  //         method: 'GET',
-  //         success(res) {
-  //           const user = res.data;
-  //           // Update local data
-  //           page.setData({
-  //             user: user
-  //           });
-
-  //           wx.hideToast();
-  //           const collection = {
-  //             user_id: current_user_id,
-  //             gif_id: data.gif.id
-  //           };
-  //           console.log(collection)
-  //           wx.request({
-  //             url: `https://gifme-api.wogengapp.cn/api/v1/users/${current_user_id}/collections`,
-  //             // url: `http://localhost:3000/api/v1/users/${current_user_id}/collections`,
-  //             method: 'POST',
-  //             data: collection,
-  //             success(e) {
-  //               console.log(e)
-  //               var cache = wx.getStorageSync('collection_id');
-  //               var currentCache = cache[this.data.currentId];
-  //               currentCache = !currentCache;
-  //               cache[this.data.currentId] = currentCache;
-  //               wx.setStorageSync('collection_id',cache);
-  //               this.setData({
-  //           collectioncheck: currentCache
-  //            });
-  //               // set data on index page and show
-  //             }
-  //           });
-  //         }
-  //       });
-  //     } else {
-  //       wx.reLaunch({
-  //         url: '../login/login'
-  //       });
-  //     }
-  //   } catch (e) {
-  //     console.log(e)
-  //   }
-  // },
-
-
   collect: function (e) {
     let data = e.currentTarget.dataset;
     let page = this;
+    page.setData({collectioncheck: true})
     try {
       var user = wx.getStorageSync('user')
       if (user) {
